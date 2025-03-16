1b915da21621b53bce69adc03507003647086ef0
saci <mouhflh37@gmail.com>
2025-03-16 01:48:15 +0100

    finish the categories section

---
+++ frontend/assets/js/shop.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("components/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar-container").innerHTML = data;
+
+      // Re-attacher les événements après le chargement du navbar
+      const mobileMenuButton = document.querySelector(
+        '[aria-controls="mobile-menu"]'
+      );
+      const mobileMenu = document.getElementById("mobile-menu");
+
+      if (mobileMenuButton) {
+        mobileMenuButton.addEventListener("click", () => {
+          mobileMenu.classList.toggle("hidden");
+        });
+      }
+
+      // Mettre à jour l'état du menu actif
+      const currentNavbarLink = document.getElementById("shop");
+      if (currentNavbarLink) {
+        currentNavbarLink.style.color = "rgb(59, 130, 246)";
+        currentNavbarLink.style.borderBottomColor = "rgb(147 197 253)";
+      }
+
+      // Update the mobile nav link
+      const mobileNavbarLink = document.getElementById("mobile-shop-link");
+      if (mobileNavbarLink) {
+        mobileNavbarLink.style.color = "rgb(59, 130, 246)";
+        mobileNavbarLink.style.backgroundColor = "rgb(249 250 251)";
+      }
+    })
+
+    .catch((error) => console.error("Error loading navbar:", error));
+
+  // Load heroShop section
+  fetch("components/shopComponents/shopHero.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("heroShop-container").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading heroShop section:", error));
+
+  // Load categoriesGrid section
+  fetch("components/shopComponents/shopGrid.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("categoriesGrid-container").innerHTML = data;
+    })
+    .catch((error) =>
+      console.error("Error loading categoriesGrid section:", error)
+    );
+
+  // load footer
+  fetch("components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      // Replace the existing content with the footer section
+      const mainContent = document.getElementById("footer-container");
+      if (mainContent) {
+        mainContent.innerHTML = data;
+
+        const backToTopButton = document.getElementById("back-to-top");
+
+        backToTopButton.addEventListener("click", () => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        });
+      } else {
+        console.error("Main content container not found");
+      }
+    })
+    .catch((error) => console.error("Error loading footer section:", error));
+});
